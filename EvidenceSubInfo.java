837b2e40a14a7d744b0f2107260d6142c7593179
ispotato <jihuiyang@126.com>
2021-04-28 15:44:26 +0800

    Create EvidenceSubInfo.java

---
+++ EvidenceSubInfo.java
@@ -0,0 +1,31 @@
+package evidence_file_tools;
+
+public class EvidenceSubInfo {
+    private String sequence;
+    private String modifications;
+    private String proteins;
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getModifications() {
+        return modifications;
+    }
+
+    public void setModifications(String modifications) {
+        this.modifications = modifications;
+    }
+
+    public String getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(String proteins) {
+        this.proteins = proteins;
+    }
+}
