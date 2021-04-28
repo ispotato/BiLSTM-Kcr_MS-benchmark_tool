9f493788008ee3966f4f73eff10bc9e89d4692ff
ispotato <jihuiyang@126.com>
2021-04-28 15:43:57 +0800

    Create Pepti31Info.java

---
+++ Pepti31Info.java
@@ -0,0 +1,31 @@
+package evidence_file_tools;
+
+public class Pepti31Info {
+    private String peptide;
+    private String proteins;
+    private String peptide31Samp;
+
+    public String getPeptide() {
+        return peptide;
+    }
+
+    public void setPeptide(String peptide) {
+        this.peptide = peptide;
+    }
+
+    public String getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(String proteins) {
+        this.proteins = proteins;
+    }
+
+    public String getPeptide31Samp() {
+        return peptide31Samp;
+    }
+
+    public void setPeptide31Samp(String peptide31Samp) {
+        this.peptide31Samp = peptide31Samp;
+    }
+}
