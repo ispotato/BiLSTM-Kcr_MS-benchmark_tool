b41c6572fda33f61dbb16985e5718177eb6318e5
ispotato <jihuiyang@126.com>
2021-04-28 15:43:15 +0800

    Create CrotKSiteInfo.java

---
+++ CrotKSiteInfo.java
@@ -0,0 +1,22 @@
+package evidence_file_tools;
+
+public class CrotKSiteInfo {
+    private int peptiSite;
+    private double probScore;
+
+    public int getPeptiSite() {
+        return peptiSite;
+    }
+
+    public void setPeptiSite(int peptiSite) {
+        this.peptiSite = peptiSite;
+    }
+
+    public double getProbScore() {
+        return probScore;
+    }
+
+    public void setProbScore(double probScore) {
+        this.probScore = probScore;
+    }
+}
